8c58ea54632c2e47375c03cef1698b46ea70e608
realrgt <ergitorubildo@gmail.com>
2019-10-05 21:39:17 +0200

    bug-fixes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
     , app = require('./config/express');
 
-http.createServer(app).listen(3000, () => {
+http.createServer(app).listen(3000, function() {
     console.log('Server running in port: ' + this.address().port);
 });
